6dce6b302b448bcca87c288abaf6474f9b5ff613
James Card <jwcard@gmail.com>
2015-12-23 21:27:08 -0500

    Add initial controller code

--- Hashsummer/src/com/gmail/jwcard/hashsummer/SummerController.java
+++ Hashsummer/src/com/gmail/jwcard/hashsummer/SummerController.java
@@ -1,5 +1,60 @@
 package com.gmail.jwcard.hashsummer;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
 public class SummerController {
-	
+
+    @FXML // fx:id="root"
+    private BorderPane root; // Value injected by FXMLLoader
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="hashResultTextArea"
+    private TextArea hashResultTextArea; // Value injected by FXMLLoader
+
+    @FXML // fx:id="clearButton"
+    private Button clearButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="algorithmButton"
+    private ChoiceBox<?> algorithmButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="stopButton"
+    private Button stopButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="statusWindow"
+    private TextField statusWindow; // Value injected by FXMLLoader
+
+    @FXML // fx:id="saveButton"
+    private Button saveButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="calcHashButton"
+    private Button calcHashButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cmpHashButton"
+    private Button cmpHashButton; // Value injected by FXMLLoader
+
+    @FXML // This method is called by the FXMLLoader when initialization is
+	  // complete
+    void initialize() {
+	assert hashResultTextArea != null : "fx:id=\"hashResultTextArea\" was not injected: check your FXML file 'Summer.fxml'.";
+	assert clearButton != null : "fx:id=\"clearButton\" was not injected: check your FXML file 'Summer.fxml'.";
+	assert algorithmButton != null : "fx:id=\"algorithmButton\" was not injected: check your FXML file 'Summer.fxml'.";
+	assert root != null : "fx:id=\"root\" was not injected: check your FXML file 'Summer.fxml'.";
+	assert stopButton != null : "fx:id=\"stopButton\" was not injected: check your FXML file 'Summer.fxml'.";
+	assert statusWindow != null : "fx:id=\"statusWindow\" was not injected: check your FXML file 'Summer.fxml'.";
+	assert saveButton != null : "fx:id=\"saveButton\" was not injected: check your FXML file 'Summer.fxml'.";
+	assert calcHashButton != null : "fx:id=\"calcHashButton\" was not injected: check your FXML file 'Summer.fxml'.";
+	assert cmpHashButton != null : "fx:id=\"cmpHashButton\" was not injected: check your FXML file 'Summer.fxml'.";
+    }
 }
