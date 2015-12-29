b539cce0b241a8df8a4d7ff9476e7c7487f5ee32
James Card <james.w.card@schneider-electric.com>
2015-12-29 02:26:28 -0500

    Reformat code

--- Hashsummer/src/com/gmail/jwcard/hashsummer/HashValue.java
+++ Hashsummer/src/com/gmail/jwcard/hashsummer/HashValue.java
@@ -16,91 +16,91 @@ import javafx.beans.property.SimpleStringProperty;
  *
  */
 public class HashValue {
-	private final SimpleStringProperty filename;
-	private SimpleStringProperty hash;
-	private final ReadOnlyLongWrapper bytesProcessed = new ReadOnlyLongWrapper();
+    private final SimpleStringProperty filename;
+    private SimpleStringProperty hash;
+    private final ReadOnlyLongWrapper bytesProcessed = new ReadOnlyLongWrapper();
 
-	/**
-	 * @param file
-	 *            Target file to generate hash on
-	 * @param algorithm
-	 *            the string representing the Java Security MessageDigest algorithm name
-	 */
-	public HashValue(File file) {
-		this.filename = new SimpleStringProperty(file.getName());
-	}
+    /**
+     * @param file
+     *            Target file to generate hash on
+     * @param algorithm
+     *            the string representing the Java Security MessageDigest algorithm name
+     */
+    public HashValue(File file) {
+        this.filename = new SimpleStringProperty(file.getName());
+    }
 
-	public final long getBytesProcessed() {
-		return bytesProcessed.get();
-	}
+    public final long getBytesProcessed() {
+        return bytesProcessed.get();
+    }
 
-	public final ReadOnlyLongProperty bytesProcessedProperty() {
-		return bytesProcessed.getReadOnlyProperty();
-	}
+    public final ReadOnlyLongProperty bytesProcessedProperty() {
+        return bytesProcessed.getReadOnlyProperty();
+    }
 
-	private long curPos;
-	private long fileLen;
+    private long curPos;
+    private long fileLen;
 
-	public long getTotalBytes() {
-		return fileLen;
-	}
+    public long getTotalBytes() {
+        return fileLen;
+    }
 
-	public String getFilename() {
-		return filename.get();
-	}
+    public String getFilename() {
+        return filename.get();
+    }
 
-	public String getHash() {
-		return hash.get();
-	}
+    public String getHash() {
+        return hash.get();
+    }
 
-	/**
-	 * Computes the hash based on the desired algorithm. algorithm is assumed to be supported by the current JCA.
-	 * 
-	 * @param file
-	 *            compute the message digest for this file
-	 * @param algorithm
-	 *            compute the message digest using this algorithm
-	 * @return the hash string or null if some error occured
-	 */
-	public String computeHash(File file, String algorithm) {
-		String hash = null;
-		try {
-			MessageDigest md = MessageDigest.getInstance(algorithm);
+    /**
+     * Computes the hash based on the desired algorithm. algorithm is assumed to be supported by the current JCA.
+     * 
+     * @param file
+     *            compute the message digest for this file
+     * @param algorithm
+     *            compute the message digest using this algorithm
+     * @return the hash string or null if some error occured
+     */
+    public String computeHash(File file, String algorithm) {
+        String hash = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance(algorithm);
 
-			fileLen = file.length();
-			long len = fileLen;
-			byte[] buffer = new byte[1024 * 1024];
-			BufferedInputStream input = new BufferedInputStream(new FileInputStream(file));
-			while (len > 0) {
-				long bytesRead = input.read(buffer, 0, buffer.length);
-				md.update(buffer, 0, (int) bytesRead);
-				curPos = curPos + bytesRead;
-				len -= bytesRead;
-				bytesProcessed.set(curPos);
-				if (SummerController.isCancelled()) {
-					break;
-				}
-			}
-			input.close();
+            fileLen = file.length();
+            long len = fileLen;
+            byte[] buffer = new byte[1024 * 1024];
+            BufferedInputStream input = new BufferedInputStream(new FileInputStream(file));
+            while (len > 0) {
+                long bytesRead = input.read(buffer, 0, buffer.length);
+                md.update(buffer, 0, (int) bytesRead);
+                curPos = curPos + bytesRead;
+                len -= bytesRead;
+                bytesProcessed.set(curPos);
+                if (SummerController.isCancelled()) {
+                    break;
+                }
+            }
+            input.close();
 
-			if (!SummerController.isCancelled()) {
-				byte[] hashValue = md.digest();
-				// convert the byte to hex format method 1
-				StringBuffer hashCodeBuffer = new StringBuffer();
-				for (int i = 0; i < hashValue.length; i++) {
-					hashCodeBuffer.append(Integer.toString((hashValue[i] & 0xff) + 0x100, 16).substring(1));
-				}
-				hash = hashCodeBuffer.toString();
-				this.hash = new SimpleStringProperty(hash);
-			} else {
-				this.hash = new SimpleStringProperty("<<Cancelled>>");
-			}
-		} catch (NoSuchAlgorithmException | IOException e) {
-			this.hash = new SimpleStringProperty(e.getMessage());
-		}
+            if (!SummerController.isCancelled()) {
+                byte[] hashValue = md.digest();
+                // convert the byte to hex format method 1
+                StringBuffer hashCodeBuffer = new StringBuffer();
+                for (int i = 0; i < hashValue.length; i++) {
+                    hashCodeBuffer.append(Integer.toString((hashValue[i] & 0xff) + 0x100, 16).substring(1));
+                }
+                hash = hashCodeBuffer.toString();
+                this.hash = new SimpleStringProperty(hash);
+            } else {
+                this.hash = new SimpleStringProperty("<<Cancelled>>");
+            }
+        } catch (NoSuchAlgorithmException | IOException e) {
+            this.hash = new SimpleStringProperty(e.getMessage());
+        }
 
-		return hash;
-	}
+        return hash;
+    }
 
     /**
      * Compares the original hash against the hash based on the desired algorithm. algorithm is assumed to be supported by the
@@ -154,9 +154,9 @@ public class HashValue {
         if (hash != null) {
             isMatch = hash.toLowerCase().equals(origHash.toLowerCase());
             if (isMatch) {
-            this.hash = new SimpleStringProperty("Matches");
+                this.hash = new SimpleStringProperty("Matches");
             } else {
-                this.hash = new SimpleStringProperty("Does not match");                
+                this.hash = new SimpleStringProperty("Does not match");
             }
         }
         return isMatch;
--- Hashsummer/src/com/gmail/jwcard/hashsummer/SummerController.java
+++ Hashsummer/src/com/gmail/jwcard/hashsummer/SummerController.java
@@ -74,21 +74,20 @@ public class SummerController {
     @FXML // fx:id="fileColumn"
     private TableColumn<?, ?> fileColumn; // Value injected by FXMLLoader
 
-	@FXML // fx:id="hashColumn"
-	private TableColumn<?, ?> hashColumn; // Value injected by FXMLLoader
-	
-	// ================ end of JavaFX declarations
+    @FXML // fx:id="hashColumn"
+    private TableColumn<?, ?> hashColumn; // Value injected by FXMLLoader
 
-	protected static ObservableList<HashValue> data = FXCollections.<HashValue> observableArrayList();
+    // ================ end of JavaFX declarations
 
-	// Only one task can run at a time since buttons are disabled once processing starts.
-	private static Task<Void> task = null;
-	
-	// indicates whether or not an error was ever generated when computing hashes
-	private boolean errorExists = false;
+    protected static ObservableList<HashValue> data = FXCollections.<HashValue> observableArrayList();
 
+    // Only one task can run at a time since buttons are disabled once processing starts.
+    private static Task<Void> task = null;
 
-	@FXML // This method is called by the FXMLLoader when initialization is complete
+    // indicates whether or not an error was ever generated when computing hashes
+    private boolean errorExists = false;
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert clearButton != null : "fx:id=\"clearButton\" was not injected: check your FXML file 'Summer.fxml'.";
         assert algorithmButton != null : "fx:id=\"algorithmButton\" was not injected: check your FXML file 'Summer.fxml'.";
@@ -113,8 +112,8 @@ public class SummerController {
         algorithmButton.setTooltip(new Tooltip("Message digest algorithm"));
 
         // Not really used at the moment
-		FilteredList<HashValue> filteredData = new FilteredList<>(data, n -> true);
-		hashTable.setItems(filteredData);
+        FilteredList<HashValue> filteredData = new FilteredList<>(data, n -> true);
+        hashTable.setItems(filteredData);
 
         fileColumn.setCellValueFactory(new PropertyValueFactory<>("filename"));
         hashColumn.setCellValueFactory(new PropertyValueFactory<>("hash"));
@@ -208,30 +207,30 @@ public class SummerController {
         th.start();
     }
 
-	@FXML
-	void doCompareHash(ActionEvent event) {
-		FileChooser fileChooser = new FileChooser();
-		fileChooser.setTitle("Compare hash");
-		String home = System.getProperty("user.home");
-		fileChooser.setInitialDirectory(new File(home));
-		fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Hash file", "*.sum"));
+    @FXML
+    void doCompareHash(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Compare hash");
+        String home = System.getProperty("user.home");
+        fileChooser.setInitialDirectory(new File(home));
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Hash file", "*.sum"));
 
-		File sumFile = fileChooser.showOpenDialog(root.getScene().getWindow());
-		if (sumFile != null) {
-			CSVReader reader = null;
-			try {
-				reader = new CSVReader(new FileReader(sumFile));
-				List<String[]> myEntries = reader.readAll();
-				handleCompare(sumFile.getParent(), myEntries);
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+        File sumFile = fileChooser.showOpenDialog(root.getScene().getWindow());
+        if (sumFile != null) {
+            CSVReader reader = null;
+            try {
+                reader = new CSVReader(new FileReader(sumFile));
+                List<String[]> myEntries = reader.readAll();
+                handleCompare(sumFile.getParent(), myEntries);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 
     /*
      * Spin off a JavaFX Task in the background to handle the comparison of hash values for the selected files.
@@ -293,23 +292,23 @@ public class SummerController {
         th.start();
     }
 
-	@FXML
-	void doSave(ActionEvent event) {
-		FileChooser fileChooser = new FileChooser();
-		fileChooser.setTitle("Save hash values");
-		String home = System.getProperty("user.home");
-		fileChooser.setInitialDirectory(new File(home));
-		fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Hash file", "*.sum"));
+    @FXML
+    void doSave(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save hash values");
+        String home = System.getProperty("user.home");
+        fileChooser.setInitialDirectory(new File(home));
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Hash file", "*.sum"));
 
-		File sumFile = fileChooser.showSaveDialog(root.getScene().getWindow());
+        File sumFile = fileChooser.showSaveDialog(root.getScene().getWindow());
         if (sumFile != null) {
             try {
                 CSVWriter writer = new CSVWriter(new FileWriter(sumFile), ',');
                 String[] entries = new String[2];
                 int rowCnt = fileColumn.getTableView().getItems().size();
                 for (int i = 0; i < rowCnt; i++) {
-                     entries[0] = (String) fileColumn.getCellData(i);
-                     entries[1] = (String) hashColumn.getCellData(i);
+                    entries[0] = (String) fileColumn.getCellData(i);
+                    entries[1] = (String) hashColumn.getCellData(i);
                     writer.writeNext(entries);
                 }
                 writer.close();
@@ -325,20 +324,20 @@ public class SummerController {
         task.cancel();
     }
 
-	@FXML
-	void doClear(ActionEvent event) {
-		data.clear();
-		saveButton.setDisable(true); // disable the save button now
-		errorExists = false; // clear the error indicator
-		statusWindow.setText("");
-	}
+    @FXML
+    void doClear(ActionEvent event) {
+        data.clear();
+        saveButton.setDisable(true); // disable the save button now
+        errorExists = false; // clear the error indicator
+        statusWindow.setText("");
+    }
 
-	/**
-	 * Allows other objects to query whether they should abort their processing
-	 * 
-	 * @return true if a cancel request has been made otherwise false
-	 */
-	static public boolean isCancelled() {
-		return task.isCancelled();
-	}
+    /**
+     * Allows other objects to query whether they should abort their processing
+     * 
+     * @return true if a cancel request has been made otherwise false
+     */
+    static public boolean isCancelled() {
+        return task.isCancelled();
+    }
 }
